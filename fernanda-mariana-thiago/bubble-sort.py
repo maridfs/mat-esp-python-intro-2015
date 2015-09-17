3986259b2055741e0014bc976d1196253abcaa5f
Mariana Santos <maridefariasantos@hotmail.com>
2015-09-17 13:41:05 -0300

    Adicionado ao código o print da lista original e da lista em ordem crescente.

--- fernanda-mariana-thiago/bubble-sort.py
+++ fernanda-mariana-thiago/bubble-sort.py
@@ -2,6 +2,7 @@
 n = 20 
 # lista contendo os elementos
 lista = [11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8] 
+print("lista original", lista)
 # i é o elemeto a ser fixado. 0 é o inicio, n-1 é ultimo elemento a ser analisado e 1 é o intervalo
 for i in range(0,n-1,1):
 # j é o elemento que será comparado com i de modo que sempre será diferente do mesmo. i+1 é o início.
@@ -12,4 +13,4 @@ for i in range(0,n-1,1):
             temp = lista[i]
             lista[i] = lista[j]	
             lista[j] = temp			
-    print [lista]
+print("lista em ordem crescente", lista)
